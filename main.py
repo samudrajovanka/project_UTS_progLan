25a32660dc9086205910260f5ae05b9d95edc9ab
Jovanka <samudrajovanka20@gmail.com>
2020-03-15 21:06:33 +0700

    update code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 '''
 Project Program Lanjut
+
 Jovanka Samudra
 Marshall Anugrah Najmi
 Daffy Ramzi
@@ -8,7 +9,6 @@ Daffy Ramzi
 # mengambil library os
 import os
 
-# popLeft = mengeluarkan array indeks ke 0 dan menghapus elemen terakhir
 def popLeft(arr):
     val=arr[0]
 
@@ -20,55 +20,54 @@ def popLeft(arr):
     arr[-1:] = []
     return val
 
-def getMid(*arrs, lengthArr, data = [], queue = []):
+def getMid(arrs, lengthArr, data = [], queue = []):
     if len(data) == lengthArr:
-        # print(data)
         return data
     else:
-        # memasukan per array ke dalam list queue (list untuk antrian pengambilan data) 
-        for arr in arrs:
-            queue.append(arr)
-
-        # mengambil data dari fungsi popLeft
-        val_pop = popLeft(queue)
-
-        # mengambil indeks tengah-tengah dari array yang di pop
-        ind_mid = (len(val_pop))//2
-
-        # memasukan nilai tengah dari array yang di pop ke list data
-        data.append(val_pop[ind_mid])
-
-        if ind_mid == 0:
-            # merekursif jika tidak ada data di kiri dan kanan
-            getMid(lengthArr = lengthArr, queue = queue)
-        elif ind_mid == 1:
-            if len(val_pop) == 3:
-                getMid(val_pop[:ind_mid], val_pop[ind_mid+1:], lengthArr = lengthArr, queue = queue)
-            else:
-            # merekursif jika hanya ada data di kiri
-                getMid(val_pop[:ind_mid], lengthArr = lengthArr, queue = queue)
+        # memasukan ke dalam list queue jika ada 2 data
+        if len(arrs) == 2:
+            for arr in arrs:
+                # memasukan arr jika panjangnya tidak 0
+                if len(arr) != 0:
+                    queue.append(arr)
+        # memasukan ke dalam list queue jika ada satu data
         else:
-            # merekursif jika ada data di kiri dan kanan
-            getMid(val_pop[:ind_mid], val_pop[ind_mid+1:], lengthArr = lengthArr, queue = queue)
+            queue.append(arrs)
 
+        # mengeluarkan list terdepatan pada list queue
+        valPop = popLeft(queue)
+        # mencari titik tengah dari valPop
+        indMid = (len(valPop))//2
+        # memasukan nilai tengah pada valPop ke list data
+        data.append(valPop[indMid])
+        # membagi dua list menjadi left dan right
+        left = valPop[:indMid]
+        right = valPop[indMid+1:]
+        arrs = [left, right]
+
+        # merekursif fungsi getMid
+        return getMid(arrs, lengthArr)
+
+# membuat main
 if __name__ == "__main__":
     # membuat fungsi untuk clear screen (anonymous function)
     cls_scr = lambda: os.system("cls")
 
+    # inisislisasi variable
     f_arr = []
     is_continue = True
 
     while is_continue :
-        limit = int(input("Masukan limit angka yang ingin dimasukan = "))
-        if limit < 21:
+        limit = int(input("Masukan limit array = "))
+        if limit < 5:
             print("Limit yang anda masukan kurang dari 21\n")
         else: 
             is_continue = False
-            for i in range (0,limit):
+            for i in range (limit):
                 f_arr.append(int(input(f"Angka elemen ke-{i+1}: ")))
 
             cls_scr()
             print("Data asli =", f_arr)
-            length = len(f_arr)
-            results = getMid(f_arr, lengthArr=length)
-            print("Data hasil = ", results)
+            length = len(f_arr) 
+            results = getMid(f_arr, length)
+            print("Data hasil =", results)
